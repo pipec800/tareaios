d14687313c3109ca0910e4fe7df599162704a982
felipe castro <felipecastro@felipes-Mac.local>
2015-10-01 22:12:28 -0300

    Initial Commit

---
+++ actividad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  actividad
+//
+//  Created by felipe castro on 01-10-15.
+//  Copyright (c) 2015 Felipe Castro Valenzuela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
